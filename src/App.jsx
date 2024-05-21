aead9b5c227e8337657b34982362822592a346d9
Shay Kelley <shaaayyk@gmail.com>
2024-05-20 21:00:29 -0500

    added bare bones for layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Layout from './layouts/Layout'
+import FeedPage from './pages/FeedPage'
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={ <FeedPage />} />
+        </Route>
+      </Routes>
     </>
   )
 }
